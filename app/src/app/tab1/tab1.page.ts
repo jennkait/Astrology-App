392eed6b3150fedd3b892ebcc68a68f829a6b6e8
Kaitlyn <jennkait@gmail.com>
2020-11-12 19:26:10 -0500

    For Brett, Forever Ago

--- app/src/app/tab1/tab1.page.ts
+++ app/src/app/tab1/tab1.page.ts
@@ -11,12 +11,15 @@ export class Tab1Page {
   // films: Observable<any>;
   data = {}
   constructor(private httpClient: HttpClient) {
-
-    this.httpClient.get('https://us-central1-astromoon.cloudfunctions.net/api/moon-sign').subscribe(data => {
-      console.log('my data: ', data);
-      // data = { sign, startTime, endTime }
-      this.data = data;
-    })
+this.data = {
+  signs: ['Virgo','Libra'],
+  startTime: '2020-10-15 05:54'
+}
+    // this.httpClient.get('https://us-central1-astromoon.cloudfunctions.net/api/moon-sign').subscribe(data => {
+    //   console.log('my data: ', data);
+    //   // data = { sign, startTime, endTime }
+    //   this.data = data;
+    // })
   }
 
 }
